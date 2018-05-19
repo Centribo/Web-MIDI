1c06770746a479d9ec0d9416f61d8c8f8dad03c0
Adam Leung <centribo@gmail.com>
2018-05-19 00:19:48 -0400

    Fixed onMIDIMessage support for multiple devices

--- scripts/MIDIController.js
+++ scripts/MIDIController.js
@@ -1,11 +1,7 @@
 var noteNames = ["C", "C#/Db", "D", "Eb/D#", "E", "F", "F#/Gb", "G", "Ab/G#", "A", "Bb/A#", "B"];
-var controllers = [];
+var controllers = {}; //Dictionary/Associative array of MIDIControllers
 
-function init(){
-	loadMIDI();
-}
-
-function loadMIDI(){
+function loadMIDIDevices(){
 	if (navigator.requestMIDIAccess) {
 		console.log("Browser supports MIDI!");
 		navigator.requestMIDIAccess().then(MIDILoadSuccess, MIDILoadFailure); //Promises
@@ -15,7 +11,6 @@ function loadMIDI(){
 }
 
 function MIDILoadSuccess(midi) {
-	this.currentlyOnNotes = [];
 	if(midi.inputs.size < 1){
 		console.log("No MIDI input devices found!");
 		return;
@@ -25,11 +20,15 @@ function MIDILoadSuccess(midi) {
 	var inputs = midi.inputs.values();
 	for (var input = inputs.next(); input && !input.done; input = inputs.next()){
 		var c = new MIDIController(input.value.name, input.value.manufacturer);
-		input.value.onmidimessage = c.onMIDIMessage;
-		controllers.push(c);
+		input.value.onmidimessage = onMIDIMessage;
+		controllers[input.value.id] = c;
 	}
 
 	console.log(controllers);
+}
+
+function onMIDIMessage(message) {
+	controllers[message.target.id].onMIDIMessage(message);
 }
 
 function MIDILoadFailure() {
@@ -48,22 +47,20 @@ class MIDIController {
 			//144 = noteOn, 128 = noteOff
 		//message.data[1] == event key
 		//message.data[2] == key velocity
-		
+
+		//Add or remove note from currentlyOnNotes array
 		if(message.data[0] == 144){
-			if(currentlyOnNotes.indexOf(message.data[1]) == -1){
-				currentlyOnNotes.push(message.data[1]);
+			if(this.currentlyOnNotes.indexOf(message.data[1]) == -1){
+				this.currentlyOnNotes.push(message.data[1]);
 			}
 		} else if(message.data[0] == 128){
-			var index = currentlyOnNotes.indexOf(message.data[1]);
+			var index = this.currentlyOnNotes.indexOf(message.data[1]);
 			if(index > -1){
-				currentlyOnNotes.splice(index, 1);
+				this.currentlyOnNotes.splice(index, 1);
 			}
 		}
 		
-		console.log(currentlyOnNotes);
-		// console.log(message.data);
-		// console.log(mapMIDItoNoteName(message.data[1]));
-		// console.log(mapMIDItoNoteFrequency(message.data[1]));
+		console.log(this.device, this.currentlyOnNotes);
 	}
 }
 
